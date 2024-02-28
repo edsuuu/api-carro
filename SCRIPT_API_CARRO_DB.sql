c43b5dd61b76cf12be63871ff2ac10eec66b7fcf
edsuu <111332935+edsuuu@users.noreply.github.com>
2024-02-28 08:05:29 -0300

    Update SCRIPT_API_CARRO_DB.sql

--- SCRIPT_API_CARRO_DB.sql
+++ SCRIPT_API_CARRO_DB.sql
@@ -21,3 +21,7 @@ insert into carros (modelo, placa) values  ('Honda Civic','ELV1590');
 #Selecionar toda a tabela 
 
 SELECT * FROM dbapicarros.carros;
+
+#alterar o usuario caso der o erro code: 'ER_NOT_SUPPORTED_AUTH_MODE',
+
+alter user 'root'@'localhost' identified with mysql_native_password by 'root';
